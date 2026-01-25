c7b0a8a9e9335692e34f68c7a1208c34b9bb232a
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2026-01-25 18:49:33 +0800

    Update artifact name (#1)
    * fix: Update artifact name

--- automatic/voicemeet/tools/chocolateyinstall.ps1
+++ automatic/voicemeet/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿
 $ErrorActionPreference = 'Stop'
-$url64      = 'https://github.com/voicemeet/releases/releases/download/v0.0.3/voicemeet-0.0.3-windows-setup-x64.exe'
+$url64      = 'https://github.com/voicemeet/releases/releases/download/v0.0.4/VoiceMeetSetup-0.0.4-x64.exe'
 
 $arch = Get-OSArchitectureWidth -Compare 64
 
@@ -16,7 +16,7 @@ $packageArgs = @{
 
   softwareName  = 'voicemeet*'
 
-  checksum64    = '25c4339970292ad54d317e3e51b8f94b10b8cdfbbd8a71cf7bb78ac16834bf3c'
+  checksum64    = 'cfaf802e96c6a666d46fc175ffa527c4ccc5c9ed543d57e24a376ba9bf5c6a89'
   checksumType64= 'sha256'
 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
