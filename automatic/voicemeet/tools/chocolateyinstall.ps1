a79fb109378a484eb8810c4432a82d40b9bec18d
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2026-04-27 19:47:31 +0800

    chore: 0.0.5-rc0

--- automatic/voicemeet/tools/chocolateyinstall.ps1
+++ automatic/voicemeet/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿
 $ErrorActionPreference = 'Stop'
-$url64      = 'https://github.com/voicemeet/releases/releases/download/v0.0.4/VoiceMeetSetup-0.0.4-x64.exe'
+$url64      = 'https://github.com/voicemeet/releases/releases/download/v0.0.5/VoiceMeetSetup-0.0.5-x64.exe'
 
 $arch = Get-OSArchitectureWidth -Compare 64
 
@@ -16,7 +16,7 @@ $packageArgs = @{
 
   softwareName  = 'voicemeet*'
 
-  checksum64    = '61d5a714944bb07dc88b45353885c41155d3b8ca29f8969468b6328a383801b7'
+  checksum64    = 'c9e5b5d29a4bf84958467ae18e36fb0676fdc81405b597912b43ab22e74f4207'
   checksumType64= 'sha256'
 
   silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
